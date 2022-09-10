c2d59d8722556f1d105b183a35cc4ba0011c47c6
BigPaxa <53959301+BigPaxa@users.noreply.github.com>
2022-09-10 05:37:22 -0400

    (mat ui)Fix

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Button } from '@mui/material';
 
 function Header() {
     return (
         <div>
+            <Button variant="contained">Hello World</Button>
+            <AccountCircleIcon fontSize='large'/>
             <h2>Hello World</h2>
         </div>
     )
